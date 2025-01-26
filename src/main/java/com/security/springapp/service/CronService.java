c10f511c656bd9978e4e56a34f8ac160c10a7ec2
rishabh1S <rishabh15112001@gmail.com>
2025-01-26 16:26:09 +0530

    feat: enhance CronService with logging

--- src/main/java/com/security/springapp/service/CronService.java
+++ src/main/java/com/security/springapp/service/CronService.java
@@ -1,26 +1,34 @@
 package com.security.springapp.service;
 
+import java.time.LocalDateTime;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
+@Service
 public class CronService {
     @Autowired
     RestTemplate restTemplate;
+    private static final Logger logger = LoggerFactory.getLogger(CronService.class);
 
     @Value("${BACKEND_URL}")
     public String BASE_URL;
 
     @Scheduled(fixedRate = 890000)
     public void pingSelf() {
+        logger.info("Attempting to ping self at: {}", LocalDateTime.now());
         try {
             String url = BASE_URL + "/api/health";
             String response = restTemplate.getForObject(url, String.class);
-            System.out.println("Ping successful: " + response);
+            logger.info("Ping successful: {}", response);
         } catch (RestClientException e) {
-            System.out.println("Failed to ping: " + e.getMessage());
+            logger.error("Failed to ping self: {}", e.getMessage());
         }
     }
 }
